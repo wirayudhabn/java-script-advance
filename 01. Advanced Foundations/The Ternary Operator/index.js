f530c54b840dfdd39cb2a640cc060feb573648ac
Wirayudha Brahmananda <wirayudhabnanda@gmail.com>
2026-03-14 14:24:11 +0800

    penambahan 4 file materi javascript advanced by scrimba

---
+++ 01. Advanced Foundations/The Ternary Operator/index.js
@@ -0,0 +1,4 @@
+const exerciseTimeMins = 40
+
+const message = exerciseTimeMins < 30 ? 'You need to try harder!' : 'Doing good!'
+console.log(message)
---
+++ 03. Function Expressions & Parameters/README.md
@@ -0,0 +1,2 @@
+# java-script-advance
+by Scrimba
